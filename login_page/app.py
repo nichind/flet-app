add687b4d5f40141bba876555bf58285c03ba8a7
nichind <danyka080@gmail.com>
2023-08-14 04:02:47 +0300

    login page cooldown

--- login_page/app.py
+++ login_page/app.py
@@ -15,7 +15,11 @@ def checkPass(userInput: str):
     else:
         return False
 
-def main(page):
+def main(page: ft.Page):
+
+    page.client_storage.clear()
+
+    page.title = 'Flet login page'
 
     page.theme = ft.Theme(color_scheme_seed='blue')
 
@@ -35,8 +39,34 @@ def main(page):
             answer.value = 'Incorrect password. Please try again.'
             button.disabled = False
             password.focus()
+
+            times = int(page.client_storage.get("try"))
+            page.client_storage.set("try", str(int(times) + 1))
+
+
+            if times + 1 >= 4:
+                page.client_storage.set("try", '0')
+                try_times = int(page.client_storage.get("try-time"))
+                page.client_storage.set("try-time", str(int(try_times) + 1))
+                button.disabled = True
+                seconds = 15 * try_times
+                while seconds > 0:
+                    answer.value = f'Incorrect password. Wait {seconds} seconds to try again.'
+                    time.sleep(1)
+                    seconds -= 1
+                    page.update()
+
+                button.disabled = False
+                answer.value = f''
+
         time.sleep(1.5)
         page.update()
+
+    if page.client_storage.contains_key("try") is False:
+        page.client_storage.set("try", "0")
+
+    if page.client_storage.contains_key("try-time") is False:
+        page.client_storage.set("try-time", "1")
 
     answer = ft.Text(value='', italic=True)
     password = ft.TextField(hint_text="password", width=250)
