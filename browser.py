b32ee8c99166eec16cfab4341315f638a656e93e
nichind <danyka080@gmail.com>
2023-09-11 23:49:46 +0300

    browser enhancement + kill process buttons

--- browser.py
+++ browser.py
@@ -1,33 +1,54 @@
 import os
-import time
+import subprocess
 import flet as ft
 import glob
+import signal
 
-def button(project):
-
-    def runProject(e):
-        while True:
-            try:
-                os.system(f'python {project}app.py')
-                break
-            except ModuleNotFoundError:
-                os.system(f'pip install flet')
-
-    return(ft.ElevatedButton(project.split('\\')[-2].upper().replace('-', ' '), on_click=runProject))
 
 def main(page: ft.Page):
+
+    class Button(object):
+        def __init__(self):
+            self.path = ''
+            self.process = None
+            self.row = None
+
+        def setpath(self, path):
+            self.path = path
+
+        def run(self):
+            def start(e):
+                self.process = subprocess.Popen(f'python {self.path}app.py')
+
+            def close(e):
+                try:
+                    os.kill(self.process.pid, signal.SIGTERM)
+                except: pass
+
+            def row():
+                gen = ft.ElevatedButton(project.split('\\')[-2].upper().replace('-', ' '), on_click=start)
+                cl = ft.ElevatedButton('Kill', on_click=close, opacity=5)
+
+                self.row = [gen, cl]
+                return self.row
+
+            return row()
 
     page.theme = ft.Theme(color_scheme_seed='green')
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-
     page.add(ft.Text('nichind flet Projects Browser.'))
 
     for project in glob.glob(".\\projects\\*\\", recursive = True):
-        page.add(button(project))
+        prj = Button()
+
+        prj.setpath(project)
+
+        page.add(ft.Row(prj.run(), alignment=ft.MainAxisAlignment.CENTER))
 
     page.title = 'Browse current flet apps in this repo'
 
 
-ft.app(target=main)
+if __name__ == '__main__':
+    ft.app(target=main)
